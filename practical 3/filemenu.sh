4c2faa298b17ce0715dab8176c7a147290a2bc78
poonamlol <poonammate.ml24@sbjit.edu.in>
2026-01-27 00:08:31 +0530

    Removed root README and extra marksheet

--- practical 3/filemenu.sh
+++ practical 3/filemenu.sh
@@ -3,7 +3,8 @@
 echo "1. Create File"
 echo "2. Write to File"
 echo "3. Append to File"
-echo "4. Delete File Content"
+echo "4. Delete File"
+echo "Enter choice:"
 read ch
 
 echo "Enter filename:"
@@ -11,9 +12,8 @@ read fname
 
 case $ch in
 1) touch $fname ;;
-2) echo "Enter content:" ; read text ; echo "$text" > $fname ;;
-3) echo "Enter content:" ; read text ; echo "$text" >> $fname ;;
-4) > $fname ;;
+2) echo "Enter content:" ; read text ; echo $text > $fname ;;
+3) echo "Enter content:" ; read text ; echo $text >> $fname ;;
+4) rm $fname ;;
 *) echo "Invalid choice" ;;
 esac
-
