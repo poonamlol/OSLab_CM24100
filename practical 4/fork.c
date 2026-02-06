95d405207aebf463b47a5bf8a311720cf1b97dfa
poonamlol <poonammate.ml24@sbjit.edu.in>
2026-02-06 23:47:26 +0530

    Added Practical 4 programs and PDFs

---
+++ practical 4/fork.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    pid_t pid = fork();
+
+    if (pid == 0) {
+        printf("Child Process\n");
+        printf("PID: %d\n", getpid());
+        printf("PPID: %d\n", getppid());
+    } else {
+        printf("Parent Process\n");
+        printf("PID: %d\n", getpid());
+        printf("Child PID: %d\n", pid);
+    }
+
+    return 0;
+}
